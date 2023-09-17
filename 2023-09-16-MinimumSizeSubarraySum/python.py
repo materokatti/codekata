04f7cd68c4f973bc5a8c4396ab033fdbdb2b59a6
Doyun_hwang <77888901+materokatti@users.noreply.github.com>
2023-09-17 19:24:40 +0900

    fix : rewrite minimus size subarray sum

--- 2023-09-16-MinimumSizeSubarraySum/python.py
+++ 2023-09-16-MinimumSizeSubarraySum/python.py
@@ -2,33 +2,28 @@ from typing import List
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        # Sort the input list nums in decreasing order
-        nums.sort(reverse=True)
+        # If the list is empty, return 0
+        if not nums:
+            return 0
         
-        # Initialize an empty list named stack
-        self.stack = []
+        # Initialize the left pointer to 0
+        left = 0
+        # Initialize a variable to keep track of the current sum of the subarray
+        current_sum = 0
+        # Initialize a variable to keep track of the minimum length of the subarray (initialized to infinity so any valid length will be smaller)
+        min_length = float('inf')
         
-        # Print the sorted list
-        print(nums)
+        # Move the right pointer from the start to the end of the array
+        for right in range(len(nums)):
+            # Add the current element to the current sum
+            current_sum += nums[right]
+            # While the current sum is greater or equal to the target, keep moving the left pointer to find the minimum length subarray
+            while current_sum >= target:
+                # Update the minimum length if a smaller length is found
+                min_length = min(min_length, right - left + 1)
+                # Subtract the value at the left pointer from the current sum and move the left pointer by one position
+                current_sum -= nums[left]
+                left += 1
         
-        # If the sum of all numbers in nums is greater than or equal to the target
-        if sum(nums) >= target:
-            
-            # Iterate through each number in nums
-            for i in nums:
-                
-                # If the current number is greater than or equal to the target, return 1 
-                # as we have found a subarray of length 1 that satisfies the condition
-                if i >= target:
-                    return 1
-                else:
-                    # If the current number is less than the target, add it to the stack
-                    self.stack.append(i)
-                
-                # If the sum of numbers in the stack is greater than or equal to the target,
-                # return the length of the stack as we have found a subarray that satisfies the condition
-                if sum(self.stack) >= target:
-                    return len(self.stack)
-        
-        # If no subarray is found that satisfies the condition, return 0
-        return 0
+        # If a valid subarray was found (min_length is not infinity), return the minimum length found, otherwise return 0 (no valid subarray found)
+        return min_length if min_length != float('inf') else 0
