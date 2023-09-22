c507ffa6ef0e0d1a9a094f55994b1f35ff3b7cf9
Doyun_hwang <77888901+materokatti@users.noreply.github.com>
2023-09-22 09:08:03 +0900

    Create : Summary Ranges

---
+++ 2023-09-22-SummaryRanges/python3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        resultArr = []
+        for i in nums :
+            
+            resultArr.append(str(i))
+
+        print(resultArr)
